f04adde937cd738ed3f0691f5b77543ffc02c6c6
narellazhang <zhangnanmei@126.com>
2019-06-13 09:50:24 +0800

    new hello

---
+++ src/main/java/com/example/springboothelloworld/controller/helloController.java
@@ -0,0 +1,12 @@
+package com.example.springboothelloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello world";
+    }
+}
